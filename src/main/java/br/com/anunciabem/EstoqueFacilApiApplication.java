d387d1ca99641abdebd583739c1f74c5b10f1128
Gabriel Honda <gabrielhondacba@gmail.com>
2024-05-08 19:41:05 -0400

    chore: initial commit 🎉

---
+++ src/main/java/br/com/anunciabem/EstoqueFacilApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.anunciabem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueFacilApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EstoqueFacilApiApplication.class, args);
+  }
+
+}
