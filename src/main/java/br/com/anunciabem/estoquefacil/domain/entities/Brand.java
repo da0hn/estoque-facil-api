193f45da6b5598bb2710a12a95aa3579fd46ba82
Gabriel Honda <gabrielhondacba@gmail.com>
2024-05-16 11:42:05 -0400

    feat: adiciona entidade `Brand`

---
+++ src/main/java/br/com/anunciabem/estoquefacil/domain/entities/Brand.java
@@ -0,0 +1,58 @@
+package br.com.anunciabem.estoquefacil.domain.entities;
+
+import br.com.anunciabem.estoquefacil.domain.constraints.ValidationUtils;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Entity(name = "Brand")
+@Table(name = "brand")
+public class Brand extends Auditable implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = -7498710341003257572L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_brands")
+  @SequenceGenerator(name = "seq_brands", sequenceName = "seq_brands", allocationSize = 1)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @NotBlank
+  @Column(name = "name", unique = true, length = 128, nullable = false)
+  private String name;
+
+  @Column(name = "description", length = 500)
+  private String description;
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.EAGER, optional = false)
+  @JoinColumn(name = "category_id", nullable = false)
+  private Category category;
+
+  public void addCategory(final Category category) {
+    ValidationUtils.requireNonNull(category, "Category cannot be null");
+    this.category = category;
+    if (this.category.getBrands().contains(this)) return;
+    this.category.addBrand(this);
+  }
+
+}
--- src/main/java/br/com/anunciabem/estoquefacil/domain/entities/Category.java
+++ src/main/java/br/com/anunciabem/estoquefacil/domain/entities/Category.java
@@ -1,11 +1,14 @@
 package br.com.anunciabem.estoquefacil.domain.entities;
 
+import br.com.anunciabem.estoquefacil.domain.constraints.ValidationUtils;
 import br.com.anunciabem.estoquefacil.exceptions.BusinessValidationException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -14,9 +17,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.flywaydb.core.internal.util.StringUtils;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Builder
@@ -42,6 +51,21 @@ public class Category extends Auditable implements Serializable {
   @Column(name = "description", length = 500)
   private String description;
 
+  @Builder.Default
+  @Fetch(FetchMode.SUBSELECT)
+  @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "category")
+  private Set<Brand> brands = new HashSet<>(0);
+
+  public void addBrand(final Brand brand) {
+    ValidationUtils.requireNonNull(brand, "Brand cannot be null");
+    if (this.brands == null) {
+      this.brands = new HashSet<>(0);
+    }
+    this.brands.add(brand);
+    if (brand.getCategory() != this) return;
+    brand.addCategory(this);
+  }
+
   public void changeName(final String name) {
     if (!StringUtils.hasText(name)) {
       throw new BusinessValidationException("Category name cannot be null or empty");
@@ -51,6 +75,25 @@ public class Category extends Auditable implements Serializable {
 
   public void changeDescription(final String description) {
     this.description = description;
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() :
+      this.getClass().hashCode();
+  }
+
+  @Override
+  public final boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    final Class<?> oEffectiveClass = o instanceof HibernateProxy ?
+      ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+    final Class<?> thisEffectiveClass = this instanceof HibernateProxy ?
+      ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) return false;
+    final Category category = (Category) o;
+    return this.id != null && Objects.equals(this.id, category.id);
   }
 
 }
