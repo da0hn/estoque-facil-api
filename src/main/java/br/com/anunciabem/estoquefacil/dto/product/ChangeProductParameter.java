b8dd3372e720d0a545777fd9e1866f2efd1e0865
Gabriel Honda <gabrielhondacba@gmail.com>
2024-05-31 09:29:35 -0400

    fix: exige que `costPrice` e `salePrice` tenha 2 casas decimais

--- src/main/java/br/com/anunciabem/estoquefacil/dto/product/ChangeProductParameter.java
+++ src/main/java/br/com/anunciabem/estoquefacil/dto/product/ChangeProductParameter.java
@@ -2,6 +2,7 @@ package br.com.anunciabem.estoquefacil.dto.product;
 
 import br.com.anunciabem.estoquefacil.domain.constraints.NullableValue;
 import br.com.anunciabem.estoquefacil.domain.constraints.UniqueProductName;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -21,8 +22,10 @@ public record ChangeProductParameter(
   @NullableValue
   String description,
   @PositiveOrZero
+  @Digits(integer = 10, fraction = 2)
   BigDecimal costPrice,
   @PositiveOrZero
+  @Digits(integer = 10, fraction = 2)
   BigDecimal salePrice,
   @NullableValue
   String imei,
--- src/main/java/br/com/anunciabem/estoquefacil/dto/product/ChangeProductRequest.java
+++ src/main/java/br/com/anunciabem/estoquefacil/dto/product/ChangeProductRequest.java
@@ -2,6 +2,7 @@ package br.com.anunciabem.estoquefacil.dto.product;
 
 import br.com.anunciabem.estoquefacil.domain.constraints.NullableValue;
 import br.com.anunciabem.estoquefacil.domain.constraints.UniqueProductName;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -18,8 +19,10 @@ public record ChangeProductRequest(
   @NullableValue
   String description,
   @PositiveOrZero
+  @Digits(integer = 10, fraction = 2)
   BigDecimal costPrice,
   @PositiveOrZero
+  @Digits(integer = 10, fraction = 2)
   BigDecimal salePrice,
   @NullableValue
   String imei,
